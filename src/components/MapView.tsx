4652e518d5c8741ebd932598f31d812430b6018a
Nilesh Kapadia <code@nileshk.com>
2018-08-09 21:57:16 -0400

    Hack for landscape mode: scroll down on clicking a feature with candidates

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -202,6 +202,15 @@ class MapView extends React.Component<{}, State> {
           // console.log('Code: ' + code);
           this.setState({selectedCode: code, selectedFeature: feature, selectedLayer: featureLayer, selectedFeatureType: this.state.selectedType});
           // console.log(code);
+          try { // HACK Scroll down in landscape mode on clicking a feature with candidates so can see results
+            if (window.matchMedia("(orientation: landscape)").matches && (window.innerWidth < 800 || window.innerHeight < 500) && window.pageYOffset < 150 && MapView.candidateCount(feature) > 0) {
+              console.log("Scrolling down");
+              window.scrollBy(0, 150);
+            }
+          } catch (e) {
+            console.log("Error in trying to scroll down");
+            console.log(e);
+          }
           return [feature, featureLayer];
         });
     }
