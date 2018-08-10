4a2277e6e3afd1a9a7806404ac2d5227cc9aa67d
Nilesh Kapadia <code@nileshk.com>
2018-08-10 17:45:16 -0400

    UI tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <div className="App-header">
+          Progressive Tsunami - Tracking Progressives All Over the Map
+        </div>
         <MapView />
       </div>
     );
